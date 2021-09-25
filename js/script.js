560c5d1119507fb4c4f5ce96c1d29db14b963632
unknown <magdrymajlo@gmail.com>
2021-09-25 09:37:25 +0200

    Dodaję pliki do repozytorium

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// console.log("Hello World");
+
+//console.log("Hiii!"); //ta komenda wyświetli "Hiii!" w konsoli
+
+
+// const x = 22;
+
+// console.log(x);
+
+// const x = 30;
+
+// console.log(x);
+
+
+// let y = 12;
+
+// // console.log(y);
+
+// let y = 123;
+
+// console.log(y);
+
